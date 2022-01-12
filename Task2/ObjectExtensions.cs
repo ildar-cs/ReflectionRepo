a2cca0ffe308f8660f578b36da18dcbd21270199
Ildar Shakirov1 <Ildar_Shakirov1@epam.com>
2022-01-12 12:21:36 +0500

    add files

--- Task2/ObjectExtensions.cs
+++ Task2/ObjectExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Task2
 {
@@ -6,12 +8,23 @@ namespace Task2
     {
         public static void SetReadOnlyProperty(this object obj, string propertyName, object newValue)
         {
-            throw new NotImplementedException();
+            var parentField = obj.GetType().BaseType.GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                        .FirstOrDefault(s => s.Name.Contains($"<{propertyName}>", StringComparison.OrdinalIgnoreCase));
+
+            if (parentField != null)
+                parentField.SetValue(obj, newValue);
+
+            var field = obj.GetType().GetFields(BindingFlags.NonPublic | BindingFlags.Instance)
+                        .FirstOrDefault(s => s.Name.Contains($"<{propertyName}>", StringComparison.OrdinalIgnoreCase));
+
+            if (field != null)
+                field.SetValue(obj, newValue);
         }
 
         public static void SetReadOnlyField(this object obj, string filedName, object newValue)
         {
-            throw new NotImplementedException();
+            var prop = obj.GetType().GetField(filedName);
+            prop.SetValue(obj, newValue);
         }
     }
 }
